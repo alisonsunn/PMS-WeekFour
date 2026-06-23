b61642ccb9a8693113de9995067c4acf7749d26d
Alison Sun <alisonsun@Alisons-MacBook-Pro.local>
2026-06-23 11:06:33 +1000

    finished reusable method to return ok or notfound status

--- PMSWeekFour/PaintManagementSystem.API/Controllers/OrdersController.cs
+++ PMSWeekFour/PaintManagementSystem.API/Controllers/OrdersController.cs
@@ -11,52 +11,47 @@ namespace PaintManagementSystem.API.Controllers
     [ApiController]
     public class OrdersController : ControllerBase
     {
-        // GetOrdersByPaintId API
-        [HttpGet("paint/{paintId}")]
-        public IActionResult GetOrdersByPaintId(int paintId)
+        // Reusable method
+        private IActionResult OkOrNotFoundOrders<T>(IEnumerable<T> orders)
         {
-            var result = PaintData.Orders.Where(order => order.OrderList.Any(orderItem => orderItem.Product.ProductId == paintId)).ToList();
+            var result = orders.ToList();
             if (result.Count != 0)
             {
                 return Ok(result);
             }
             return NotFound();
         }
+
+        // GetOrdersByPaintId API
+        [HttpGet("paint/{paintId}")]
+        public IActionResult GetOrdersByPaintId(int paintId)
+        {
+            var result = PaintData.Orders.Where(order => order.OrderList.Any(orderItem => orderItem.Product.ProductId == paintId));
+            return OkOrNotFoundOrders(result);
+        }
         
         // GetOrdersByUserId API
         [HttpGet("user/{userId}")]
         public IActionResult GetOrdersByUserId(int userId)
         {
-            var result = PaintData.Orders.Where(order => order.UserId == userId).ToList();
-            if (result.Count == 0)
-            {
-                return NotFound();
-            }
-            return Ok(result);
+            var result = PaintData.Orders.Where(order => order.UserId == userId);
+            return OkOrNotFoundOrders(result);
         }
         
         // GetOrdersByDate API
         [HttpGet("createDate/{date}")]
         public IActionResult GetOrdersByDate(DateTime date)
         {
-            var result = PaintData.Orders.Where(order => order.CreatedAt.Date == date.Date).ToList();
-            if (result.Count == 0)
-            {
-                return NotFound();
-            }
-            return Ok(result);
+            var result = PaintData.Orders.Where(order => order.CreatedAt.Date == date.Date);
+            return OkOrNotFoundOrders(result);
         }
 
         // GetOrdersByPriceRange API
         [HttpGet("by-price-range")]
         public IActionResult GetOrdersByPriceRange(decimal min, decimal max)
         {
-            var result = PaintData.Orders.Where(order => order.TotalPrice > min && order.TotalPrice < max).ToList();
-            if (result.Count == 0)
-            {
-                return NotFound();
-            }
-            return Ok(result);
+            var result = PaintData.Orders.Where(order => order.TotalPrice > min && order.TotalPrice < max);
+            return OkOrNotFoundOrders(result);
         }
 
         // GetLastMonthOrders API
@@ -64,12 +59,8 @@ namespace PaintManagementSystem.API.Controllers
         public IActionResult GetLastMonthOrders()
         {
             var lastMonth = DateTime.Now.AddMonths(-1);
-            var result = PaintData.Orders.Where(order => order.CreatedAt >= lastMonth).ToList();
-            if (result.Count == 0)
-            {
-                return NotFound();
-            }
-            return Ok(result);
+            var result = PaintData.Orders.Where(order => order.CreatedAt >= lastMonth);
+            return OkOrNotFoundOrders(result);
         }
 
         // GetAllOrders API
@@ -92,14 +83,9 @@ namespace PaintManagementSystem.API.Controllers
             {
                 return BadRequest("Page number and page size must be greater than 0.");
             }
-            var result = PaintData.Orders.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            var result = PaintData.Orders.Skip((pageNumber - 1) * pageSize).Take(pageSize);
 
-            if (result.Count == 0)
-            {
-                return NotFound("No orders");
-            }
-
-            return Ok(result);
+            return OkOrNotFoundOrders(result);
         }
 
     }
