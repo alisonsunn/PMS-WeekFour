3918648ea14b68b75277c91861be042744734c30
Alison Sun <alisonsun@Alisons-MacBook-Pro.local>
2026-07-13 21:10:57 +1000

    add three layers and IPaintProductsService

--- PaintManagementSystem.API/Program.cs
+++ PaintManagementSystem.API/Program.cs
@@ -1,9 +1,15 @@
 using PaintManagementSystem.API.DataBase;
 using Microsoft.EntityFrameworkCore;
+using PaintManagementSystem.API.Repositories;
+using PaintManagementSystem.API.Services;
+using PaintManagementSystem.API.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<PaintProductRepository>();
+builder.Services.AddScoped< IPaintProductsService, PaintProductsService>();
 
 builder.Services.AddDbContext<PaintDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PaintManagementConnection")));
 
